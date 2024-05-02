a93db1a1c6e6f3689b3e8059c89e28023deb418b
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 13:50:48 +0200

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoadingScreen from './components/LoadingScreen';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const MainApp = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleAnimationEnd = () => {
+    setLoading(false);
+  };
+
+  return (
+    <React.StrictMode>
+      {loading ? (
+        <LoadingScreen onAnimationEnd={handleAnimationEnd} />
+      ) : (
+        <App />
+      )}
+    </React.StrictMode>
+  );
+};
+
+root.render(<MainApp />);
 reportWebVitals();
