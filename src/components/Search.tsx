250a2925d06a30636d16d62225e13956b89647be
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 11:33:36 +0200

    input type search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Search: React.FC = () => {
     return (
         <div className="search-container">
-            <input type="text" className="search-input" placeholder="Pretraži..." />
+            <input type="search" className="search-input" placeholder="Pretraži..." />
         </div>
     );
 };
