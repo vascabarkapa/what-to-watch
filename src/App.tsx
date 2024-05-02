21bbc34143763712eca8f919751ebec357281c90
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 09:22:39 +0200

    search css

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import TVShows from './pages/TVShows';
 import Movies from './pages/Movies';
 import Tabs from './components/Tabs';
+import Search from './components/Search';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='container'>
         <Tabs />
+        <Search />
         <Routes>
           <Route path="/tv-shows" Component={TVShows} />
           <Route path="/movies" Component={Movies} />
