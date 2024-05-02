1949266233facd09d0c084856aee6efa0d5c12a7
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 08:22:51 +0200

    app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Link } from 'react-router-dom';
+import TVShows from './pages/TVShows';
+import Movies from './pages/Movies';
 
 function App() {
   return (
-    <div>It works!</div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tv-shows">TV Shows</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/tv-shows" Component={TVShows} />
+          <Route path="/movies" Component={Movies} />
+          <Route path="/" element={<Navigate to="/tv-shows" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
