e0ce99e9f00dcd5206dcc27ca32ce7fdd6d92c6d
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 18:35:50 +0200

    card desc

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,12 +6,14 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ media }) => {
-    
+
     return (
         <div className="card">
             <img src={'https://image.tmdb.org/t/p/original/' + media?.backdrop_path} alt="Slika filma" />
-            <h2> {(media as Movie).title || (media as TVShow).name}</h2>
-            <span>{media.vote_average.toFixed(2)} ({media.vote_count})</span>
+            <div className="title-wrapper">
+                <h2>{(media as Movie).title || (media as TVShow).name}</h2>
+                <span>{media.vote_average.toFixed(2)} ({media.vote_count})</span>
+            </div>
             <p className="test">{media.overview}</p>
         </div>
     );
