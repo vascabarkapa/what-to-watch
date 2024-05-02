29d19976318594636f151283180c0d54a590e532
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 08:22:41 +0200

    added movies and tv show pages

---
+++ src/pages/Movies.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Movies: React.FC = () => {
+    return (
+        <div>
+            <h2>Movies</h2>
+            <p>This is the content of Movies.</p>
+        </div>
+    );
+};
+
+export default Movies;
---
+++ src/pages/TVShows.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TVShows: React.FC = () => {
+    return (
+        <div>
+            <h2>TV Shows Content</h2>
+            <p>This is the content of TV Shows.</p>
+        </div>
+    );
+};
+
+export default TVShows;
