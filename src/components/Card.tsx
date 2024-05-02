b71795cd39cee498c77ce59cf7cdd5750c66490d
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 18:25:52 +0200

    two lines desc

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,6 +11,8 @@ const Card: React.FC<CardProps> = ({ media }) => {
         <div className="card">
             <img src={'https://image.tmdb.org/t/p/original/' + media?.backdrop_path} alt="Slika filma" />
             <h2> {(media as Movie).title || (media as TVShow).name}</h2>
+            <span>{media.vote_average.toFixed(2)} ({media.vote_count})</span>
+            <p className="test">{media.overview}</p>
         </div>
     );
 };
