ac845a8fe849189c8b66295ce24a30322639b011
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 09:24:57 +0200

    card css

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card: React.FC = () => {
     return (
-        <div>
-            <img src="" alt="" />
+        <div className="card">
+            <img src="putanja_do_slike" alt="Slika filma" />
             <h2>Naziv filma</h2>
         </div>
     );
--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -7,6 +7,13 @@ const Movies: React.FC = () => {
             <Card />
             <Card />
             <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
         </div>
     );
 };
