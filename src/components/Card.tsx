33e96519848736d409e107704c45ff00fe84624e
vascabarkapa <vascabarkapa@gmail.com>
2024-05-02 17:09:03 +0200

    WIP card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,14 @@
-const Card = () => {
+import { Movie } from "../models/movie";
+
+interface CardProps {
+    movie: Movie;
+}
+
+const Card: React.FC<CardProps> = ({ movie }) => {
     return (
         <div className="card">
-            <img src="putanja_do_slike" alt="Slika filma" />
-            <h2>Naziv filma</h2>
+            <img src={'https://image.tmdb.org/t/p/original/' + movie?.backdrop_path} alt="Slika filma" />
+            <h2>{movie?.original_title}</h2>
         </div>
     );
 };
