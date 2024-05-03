f0f73a15a577f7c96c4e3c739d14df12592038aa
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 16:14:22 +0200

    details load main infos

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,38 +1,75 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import w2wLogo from "./../assets/w2w-logo.png";
 import LeftArrow from "../components/LeftArrow";
 import Star from "../components/Star";
+import { useEffect, useState } from "react";
+import MovieService from "../services/movieService";
+import TvShowService from "../services/tvShowService";
+import ImageHelper from "../utils/ImageHelper";
+import TVShow from "../models/tvShow";
+import Movie from "../models/movie";
+import Media from "../models/media";
 
 const Details = () => {
+    const location = useLocation();
     const navigate = useNavigate();
+
+    const { pathname } = location;
+    const isMovies = pathname.includes('movies');
+    const { id } = useParams<{ id: string }>();
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [media, setMedia] = useState({} as Media);
 
     function navigateBack() {
         navigate("/tv-shows");
     }
 
+    useEffect(() => {
+        setIsLoading(true);
+        if (id) {
+            if (isMovies) {
+                MovieService.getMovieDetails(+id).then((response) => {
+                    if (response) {
+                        setMedia(response as Movie);
+                        setIsLoading(false);
+                    }
+                });
+            } else {
+                TvShowService.getTvShowDetails(+id).then((response) => {
+                    if (response) {
+                        setMedia(response as TVShow);
+                        setIsLoading(false);
+                    }
+                });
+            }
+        }
+    }, []);
+
     return (
-        <div className="container my-50">
-            <div className="back-container">
-                <button className="back" onClick={() => navigateBack()}>
-                    <LeftArrow />&nbsp;&nbsp;Back
-                </button>
-                <img className="back-img" src={w2wLogo} alt="W2W Logo" />
-            </div>
-            <div className="details">
-                <img src={'https://image.tmdb.org/t/p/original/AvsNXUbP7UKCVnypyx2eWp8z2N3.jpg'} alt="Slika filma" loading="eager" />
-                <div className="title-wrapper">
-                    <h2>Naslov</h2>
-                    <span className="d-flex align-items-center"><Star />&nbsp;2.55 (12555)</span>
+        !isLoading ?
+            <div className="container my-50">
+                <div className="back-container">
+                    <button className="back" onClick={() => navigateBack()}>
+                        <LeftArrow />&nbsp;&nbsp;Back
+                    </button>
+                    <img className="back-img" src={w2wLogo} alt="W2W Logo" />
                 </div>
-                <div>Action, Thriller, Comedia</div>
-                <div className="d-flex">
-                    <div className="poster">
-                    <img src={'https://image.tmdb.org/t/p/original/ojDg0PGvs6R9xYFodRct2kdI6wC.jpg'} alt="Slika filma" loading="eager" />
+                <div className="details">
+                    <img src={ImageHelper.generateBackdropLink(media?.backdrop_path)} alt={media?.backdrop_path} loading="eager" />
+                    <div className="title-wrapper">
+                        <h2>{(media as TVShow).name || (media as Movie).title}</h2>
+                        <span className="d-flex align-items-center"><Star />&nbsp;{media?.vote_average} ({media?.vote_count})</span>
                     </div>
-                    <div className="overview">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                    <div>{media?.genres?.map(genre => genre.name).join(', ')}</div>
+                    <div className="d-flex">
+                        <div className="poster">
+                            <img src={ImageHelper.generateBackdropLink(media?.poster_path)} alt={media?.poster_path} loading="eager" />
+                        </div>
+                        <div className="overview">{media?.overview}</div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div> : <></>
     );
 };
 
