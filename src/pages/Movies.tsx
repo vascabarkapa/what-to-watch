a0de8f87c633f03ec6fc02246ebde903aef64eb7
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 17:23:59 +0200

    search apis implemented

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -5,23 +5,49 @@ import Movie from "../models/movie";
 import Logo from "../components/Logo";
 import Tabs from "../components/Tabs";
 import Search from "../components/Search";
+import SearchService from "../services/searchService";
 
 const Movies = () => {
+    const [searchQuery, setSearchQuery] = useState('');
     const [movies, setMovies] = useState([] as Movie[]);
 
     useEffect(() => {
+        if (searchQuery.length >= 3) {
+            const timer = setTimeout(() => {
+                fetchSearchResults();
+            }, 1000);
+
+            return () => clearTimeout(timer);
+        } else {
+            fetchTop10();
+        }
+    }, [searchQuery]);
+
+    const fetchSearchResults = async () => {
+        SearchService.searchMoviesbyTitle(searchQuery).then((response) => {
+            if (response) {
+                setMovies(response.results);
+            }
+        });
+    };
+
+    const fetchTop10 = async () => {
         MovieService.getTopRatedMovies().then((response) => {
             if (response) {
                 setMovies(response.results.slice(0, 10));
             }
-        })
-    }, []);
+        });
+    };
+
+    const handleInputChange = (value: string) => {
+        setSearchQuery(value);
+    };
 
     return (
         <div className='container'>
             <Logo />
             <Tabs />
-            <Search />
+            <Search value={searchQuery} onChange={handleInputChange} />
             <div className="card-container">
                 {movies.map((movie) => (
                     <Card key={movie.id} media={movie} />
--- src/pages/TVShows.tsx
+++ src/pages/TVShows.tsx
@@ -5,23 +5,49 @@ import TVShow from "../models/tvShow";
 import Logo from "../components/Logo";
 import Tabs from "../components/Tabs";
 import Search from "../components/Search";
+import SearchService from "../services/searchService";
 
 const TVShows = () => {
+    const [searchQuery, setSearchQuery] = useState('');
     const [tvShows, setTvShows] = useState([] as TVShow[]);
 
     useEffect(() => {
+        if (searchQuery.length >= 3) {
+            const timer = setTimeout(() => {
+                fetchSearchResults();
+            }, 1000);
+
+            return () => clearTimeout(timer);
+        } else {
+            fetchTop10();
+        }
+    }, [searchQuery]);
+
+    const fetchSearchResults = async () => {
+        SearchService.searchTvShowsbyTitle(searchQuery).then((response) => {
+            if (response) {
+                setTvShows(response.results);
+            }
+        });
+    };
+
+    const fetchTop10 = async () => {
         TvShowService.getTopRatedTvShows().then((response) => {
             if (response) {
                 setTvShows(response.results.slice(0, 10));
             }
-        })
-    }, []);
+        });
+    };
+
+    const handleInputChange = (value: string) => {
+        setSearchQuery(value);
+    };
 
     return (
         <div className='container'>
             <Logo />
             <Tabs />
-            <Search />
+            <Search value={searchQuery} onChange={handleInputChange} />
             <div className="card-container">
                 {tvShows.map((tvShow) => (
                     <Card key={tvShow.id} media={tvShow} />
