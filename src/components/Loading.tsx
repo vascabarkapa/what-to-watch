f020b3d2b09f56b4c3d8a55992c7a7a767be7fac
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 22:26:22 +0200

    loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import tvLogo from "./../assets/tv-logo.png";
+
+const Loading = () => {
+    return (
+        <div className="logo">
+            <img className="bounce" src={tvLogo} alt="TV Logo" />
+        </div>
+    );
+};
+
+export default Loading;
