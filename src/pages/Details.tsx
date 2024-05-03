1c83bac28c4f02b13ab6052f0223509107cf5458
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 14:04:49 +0200

    back button

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,14 @@
+import w2wLogo from "./../assets/w2w-logo.png";
+
 const Details = () => {
     return (
-        <div>
-            Details
+        <div className="container mt-50">
+            <div>
+                <button className="back">
+                    &larr; Back
+                </button>
+                <img className="back-img" src={w2wLogo} alt="W2W Logo" />
+            </div>
         </div>
     );
 };
