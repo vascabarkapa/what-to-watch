e71095e16aaa56fa663c353a3e31853f6c7c8c85
vascabarkapa <vascabarkapa@gmail.com>
2024-05-04 00:14:12 +0200

    trailer component

---
+++ src/components/Trailer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {
+    code: string;
+}
+
+const Trailer: React.FC<Props> = ({ code }) => {
+    return (
+        <iframe
+            src={`https://www.youtube.com/embed/${code}`}
+            title="YouTube video player" frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
+        </iframe>
+    );
+};
+
+export default Trailer;
