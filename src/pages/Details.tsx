157b21a670d7195fba5b8b69dad5998ee6fc9151
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 14:08:14 +0200

    back to

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import w2wLogo from "./../assets/w2w-logo.png";
 
 const Details = () => {
+    const navigate = useNavigate();
+
+    function navigateBack() {
+        navigate("/tv-shows");
+    }
+
     return (
         <div className="container mt-50">
-            <div>
-                <button className="back">
+            <div className="back-container">
+                <button className="back" onClick={() => navigateBack()}>
                     &larr; Back
                 </button>
                 <img className="back-img" src={w2wLogo} alt="W2W Logo" />
