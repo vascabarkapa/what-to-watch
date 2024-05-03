5260b4185b472f01d6311261d16f846c908c87ad
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 17:23:36 +0200

    search component without URL

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,29 +1,15 @@
-import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
-const Search = () => {
+type SearchInputProps = {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+const Search: React.FC<SearchInputProps> = ({ value, onChange }) => {
     const location = useLocation();
-    const [query, setQuery] = useState('');
 
-    useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const queryParam = searchParams.get('query');
-        setQuery(queryParam || '');
-    }, [location.search]);
-
-    const handleQueryChange = (event: any) => {
-        const inputValue = event.target.value.trim();
-        const searchParams = new URLSearchParams(location.search);
-
-        if (inputValue !== '') {
-            searchParams.set('query', inputValue);
-        } else {
-            searchParams.delete('query');
-        }
-
-        const newUrl = location.pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
-        window.history.pushState(null, '', newUrl);
-        setQuery(event.target.value);
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(event.target.value);
     };
 
     if (location.pathname === "/404") {
@@ -32,7 +18,7 @@ const Search = () => {
 
     return (
         <div className="search-container">
-            <input type="search" value={query} onChange={handleQueryChange} className="search-input" placeholder="Search by title..." />
+            <input type="search" value={value} onChange={handleInputChange} className="search-input" placeholder="Search by title..." />
         </div>
     );
 };
