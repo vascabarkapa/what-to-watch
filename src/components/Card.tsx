eac46508d1ecd275d149d760e90c49ce1c4576c6
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 13:07:59 +0200

    eager

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ const Card: React.FC<CardProps> = ({ media }) => {
 
     return (
         <div className="card">
-            <img src={ImageHelper.generateBackdropLink(media?.backdrop_path)} alt="Slika filma" loading="lazy" />
+            <img src={ImageHelper.generateBackdropLink(media?.backdrop_path)} alt="Slika filma" loading="eager" />
             <div className="title-wrapper">
                 <h2>{(media as Movie).title || (media as TVShow).name}</h2>
                 <span><Star />&nbsp;{media.vote_average.toFixed(2)} ({media.vote_count})</span>
