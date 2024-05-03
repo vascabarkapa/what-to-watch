f3bf756b252a219dc7268bf83fcd342d928c53ca
vascabarkapa <vascabarkapa@gmail.com>
2024-05-03 12:55:33 +0200

    details page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Tabs from './components/Tabs';
 import Search from './components/Search';
 import Logo from './components/Logo';
 import NotFound from './pages/NotFound';
+import Details from './pages/Details';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/tv-shows" Component={TVShows} />
           <Route path="/movies" Component={Movies} />
+          <Route path="/details/:id" Component={Details} />
           <Route path="/404" Component={NotFound} />
           <Route path="/" element={<Navigate to="/tv-shows" />} />
           <Route path="*" element={<Navigate to="/404" />} />
