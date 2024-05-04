2766d68b578078022fa1fd12793869454fb005fa
vascabarkapa <vascabarkapa@gmail.com>
2024-05-04 22:13:58 +0200

    optimized movies and tvshow

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,83 +1,82 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/root";
+import { setText } from "../redux/actions";
 import Card from "../components/Card";
+import SearchService from "../services/searchService";
 import MovieService from "../services/movieService";
 import Movie from "../models/movie";
 import Logo from "../components/Logo";
 import Tabs from "../components/Tabs";
 import Search from "../components/search/Search";
-import SearchService from "../services/searchService";
 import Loading from "../components/loading/Loading";
 import SearchNotFound from "../components/search/SearchNotFound";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../redux/root";
-import { setText } from "../redux/actions";
 
 const Movies = () => {
     const dispatch = useDispatch();
     const search = useSelector((state: RootState) => state.text);
-    const [movies, setMovies] = useState([] as Movie[]);
-    const [top10Movies, setTop10Movies] = useState([] as Movie[]);
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [top10Movies, setTop10Movies] = useState<Movie[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+    const fetchSearchResults = useCallback(async () => {
         setIsLoading(true);
-        if (search.text.length >= 3) {
-            const timer = setTimeout(() => {
-                fetchSearchResults();
-            }, 1000);
-
-            return () => clearTimeout(timer);
-        } else {
-            if (top10Movies.length > 0) {
-                setMovies(top10Movies);
-                setIsLoading(false);
-            } else {
-                fetchTop10();
-            }
+        try {
+            const response = await SearchService.searchMoviesbyTitle(search.text);
+            setMovies(response.results);
+        } catch (error) {
+            console.error('Error fetching search results:', error);
+        } finally {
+            setIsLoading(false);
         }
     }, [search.text]);
 
-    const fetchSearchResults = async () => {
-        SearchService.searchMoviesbyTitle(search.text).then((response) => {
-            if (response) {
-                setMovies(response.results);
-                setIsLoading(false);
-            }
-        });
-    };
+    const fetchTop10 = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const response = await MovieService.getTopRatedMovies();
+            const top10 = response.results.slice(0, 10);
+            setMovies(top10);
+            setTop10Movies(top10);
+        } catch (error) {
+            console.error('Error fetching top 10 movies:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
 
-    const fetchTop10 = async () => {
-        MovieService.getTopRatedMovies().then((response) => {
-            if (response) {
-                setMovies(response.results.slice(0, 10));
-                setTop10Movies(response.results.slice(0, 10));
-                setIsLoading(false);
-            }
-        });
-    };
+    useEffect(() => {
+        if (search.text.length >= 3) {
+            const timer = setTimeout(fetchSearchResults, 1000);
+            return () => clearTimeout(timer);
+        } else if (top10Movies.length > 0) {
+            setMovies(top10Movies);
+            setIsLoading(false);
+        } else {
+            fetchTop10();
+        }
+    }, [search.text, fetchSearchResults, fetchTop10, top10Movies.length]);
 
-    const handleInputChange = (value: string) => {
+    const handleInputChange = useCallback((value: string) => {
         dispatch(setText(value));
-    };
+    }, [dispatch]);
 
     return (
         <div className='container'>
             <Logo />
             <Tabs />
             <Search value={search.text} onChange={handleInputChange} />
-            {
-                isLoading ?
-                    <Loading />
-                    :
-                    movies.length > 0 ?
-                        <div className="card-container">
-                            {movies.map((movie) => (
-                                <Card key={movie.id} media={movie} />
-                            ))}
-                        </div>
-                        :
-                        <SearchNotFound media={'Movies'} />
-            }
+            {isLoading ? (
+                <Loading />
+            ) : (
+                movies.length > 0 ? (
+                    <div className="card-container">
+                        {movies.map(movie => <Card key={movie.id} media={movie} />)}
+                    </div>
+                ) : (
+                    <SearchNotFound media={'Movies'} />
+                )
+            )}
         </div>
     );
 };
--- src/pages/TVShows.tsx
+++ src/pages/TVShows.tsx
@@ -1,83 +1,82 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/root";
+import { setText } from "../redux/actions";
 import Card from "../components/Card";
+import SearchService from "../services/searchService";
 import TvShowService from "../services/tvShowService";
 import TVShow from "../models/tvShow";
 import Logo from "../components/Logo";
 import Tabs from "../components/Tabs";
 import Search from "../components/search/Search";
-import SearchService from "../services/searchService";
 import Loading from "../components/loading/Loading";
 import SearchNotFound from "../components/search/SearchNotFound";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../redux/root";
-import { setText } from "../redux/actions";
 
 const TVShows = () => {
     const dispatch = useDispatch();
     const search = useSelector((state: RootState) => state.text);
-    const [tvShows, setTvShows] = useState([] as TVShow[]);
-    const [top10TvShows, setTop10TvShows] = useState([] as TVShow[]);
+    const [tvShows, setTvShows] = useState<TVShow[]>([]);
+    const [top10TvShows, setTop10TvShows] = useState<TVShow[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+    const fetchSearchResults = useCallback(async () => {
         setIsLoading(true);
-        if (search.text.length >= 3) {
-            const timer = setTimeout(() => {
-                fetchSearchResults();
-            }, 1000);
-
-            return () => clearTimeout(timer);
-        } else {
-            if (top10TvShows.length > 0) {
-                setTvShows(top10TvShows);
-                setIsLoading(false);
-            } else {
-                fetchTop10();
-            }
+        try {
+            const response = await SearchService.searchTvShowsbyTitle(search.text);
+            setTvShows(response.results);
+        } catch (error) {
+            console.error('Error fetching search results:', error);
+        } finally {
+            setIsLoading(false);
         }
     }, [search.text]);
 
-    const fetchSearchResults = async () => {
-        SearchService.searchTvShowsbyTitle(search.text).then((response) => {
-            if (response) {
-                setTvShows(response.results);
-                setIsLoading(false);
-            }
-        });
-    };
+    const fetchTop10 = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const response = await TvShowService.getTopRatedTvShows();
+            const top10 = response.results.slice(0, 10);
+            setTvShows(top10);
+            setTop10TvShows(top10);
+        } catch (error) {
+            console.error('Error fetching top 10 tv shows:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
 
-    const fetchTop10 = async () => {
-        TvShowService.getTopRatedTvShows().then((response) => {
-            if (response) {
-                setTvShows(response.results.slice(0, 10));
-                setTop10TvShows(response.results.slice(0, 10));
-                setIsLoading(false);
-            }
-        });
-    };
+    useEffect(() => {
+        if (search.text.length >= 3) {
+            const timer = setTimeout(fetchSearchResults, 1000);
+            return () => clearTimeout(timer);
+        } else if (top10TvShows.length > 0) {
+            setTop10TvShows(top10TvShows);
+            setIsLoading(false);
+        } else {
+            fetchTop10();
+        }
+    }, [search.text, fetchSearchResults, fetchTop10, top10TvShows.length]);
 
-    const handleInputChange = (value: string) => {
+    const handleInputChange = useCallback((value: string) => {
         dispatch(setText(value));
-    };
+    }, [dispatch]);
 
     return (
         <div className='container'>
             <Logo />
             <Tabs />
             <Search value={search.text} onChange={handleInputChange} />
-            {
-                isLoading ?
-                    <Loading />
-                    :
-                    tvShows.length > 0 ?
-                        <div className="card-container">
-                            {tvShows.map((tvShow) => (
-                                <Card key={tvShow.id} media={tvShow} />
-                            ))}
-                        </div>
-                        :
-                        <SearchNotFound media={'TV Shows'} />
-            }
+            {isLoading ? (
+                <Loading />
+            ) : (
+                tvShows.length > 0 ? (
+                    <div className="card-container">
+                        {tvShows.map(tvShow => <Card key={tvShow.id} media={tvShow} />)}
+                    </div>
+                ) : (
+                    <SearchNotFound media={'TV Shows'} />
+                )
+            )}
         </div>
     );
 };
