0b8b24e7a516bd1eba5004caf4f1e0c87f65da0a
vascabarkapa <vascabarkapa@gmail.com>
2024-05-04 19:41:22 +0200

    not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,11 @@
-import tvLogo from "./../assets/logo/tv-logo.png";
+import { FC } from "react";
 import { Link } from "react-router-dom";
+import tvLogo from "./../assets/logo/tv-logo.png";
 
-const NotFound = () => {
+const NotFound: FC = () => {
     return (
         <div className="not-found">
-            <img src={tvLogo} alt="tv logo" width={'50%'} />
+            <img src={tvLogo} alt="tv logo" style={{ width: '50%' }} />
             <div className="not-found-text">404 | Not Found</div>
             <Link className="not-found-button" to="/">
                 Back to Home
