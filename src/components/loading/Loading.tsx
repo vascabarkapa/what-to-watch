9ffb4fecde17dce58e14dcf6297281d1c46a2bda
vascabarkapa <vascabarkapa@gmail.com>
2024-05-04 19:25:51 +0200

    optimized loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,6 +1,7 @@
+import { FC } from "react";
 import tvLogo from "./../../assets/logo/tv-logo.png";
 
-const Loading = () => {
+const Loading: FC = () => {
     return (
         <div className="logo">
             <img className="bounce" src={tvLogo} alt="TV Logo" />
--- src/components/loading/LoadingScreen.tsx
+++ src/components/loading/LoadingScreen.tsx
@@ -1,6 +1,11 @@
+import { FC, HTMLAttributes } from "react";
 import w2wLogo from "./../../assets/logo/w2w-logo.png";
 
-const LoadingScreen = ({ onAnimationEnd }: { onAnimationEnd: () => void }) => (
+type Props = HTMLAttributes<HTMLDivElement> & {
+    onAnimationEnd: () => void;
+};
+
+const LoadingScreen: FC<Props> = ({ onAnimationEnd }) => (
     <div className="loading-screen" onAnimationEnd={onAnimationEnd}>
         <div className="logo">
             <img src={w2wLogo} width={'80%'} alt="W2W Logo" />
