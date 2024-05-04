ac5524f9302f2c701bfbade033d999d21d741274
vascabarkapa <vascabarkapa@gmail.com>
2024-05-04 19:58:42 +0200

    index optimized

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import LoadingScreen from './components/loading/LoadingScreen';
 import { createStore } from 'redux';
 import rootReducer from './redux/root';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
 const store = createStore(rootReducer);
 
-const MainApp = () => {
-  const [loading, setLoading] = useState(true);
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+const MainApp: FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
+    const timer = setTimeout(() => setLoading(false), 2000);
     return () => clearTimeout(timer);
   }, []);
-
-  const handleAnimationEnd = () => {
-    setLoading(false);
-  };
 
   return (
     <React.StrictMode>
       {loading ? (
-        <LoadingScreen onAnimationEnd={handleAnimationEnd} />
+        <LoadingScreen onAnimationEnd={() => setLoading(false)} />
       ) : (
         <Provider store={store}>
           <App />
@@ -43,4 +34,3 @@ const MainApp = () => {
 };
 
 root.render(<MainApp />);
-reportWebVitals();
