622490e2896e0070b69ac8b59e6a431d417f7dba
vascabarkapa <vascabarkapa@gmail.com>
2024-05-07 22:23:05 +0200

    custom pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import ReactPaginate from 'react-paginate';
+
+type Props = {
+    handlePageClick: (event: any) => Promise<void>;
+    pageCount: number;
+    forcePage: number;
+}
+
+const Pagination: FC<Props> = ({
+    handlePageClick,
+    pageCount,
+    forcePage,
+}) => {
+    return (
+        <div className="pagination-container">
+            <ReactPaginate
+                forcePage={forcePage}
+                breakLabel="..."
+                nextLabel={null}
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel={null}
+                containerClassName={"pagination"}
+                pageClassName={"page-item"}
+                activeClassName={"active"}
+            />
+        </div>
+    );
+};
+
+export default Pagination;
