787fbd2eecec8097eeaa62df9e93f846c0f40266
vascabarkapa <vascabarkapa@gmail.com>
2024-05-07 23:07:30 +0200

    added genres to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,6 +7,8 @@ import Media from "../models/media";
 
 import imageNotAvailable from "./../assets/images/image_not_available.png";
 import { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/root";
 
 type Props = {
     media: Movie | TVShow;
@@ -14,7 +16,10 @@ type Props = {
 
 const Card: FC<Props> = ({ media }) => {
     const navigate = useNavigate();
+    const genres = useSelector((state: RootState) => state.text);
+
     const isMovie = (media: Media): media is Movie => 'title' in media;
+    const filteredGenres = isMovie(media) ? genres.movieGenres.filter(genre => media?.genre_ids?.includes(genre.id)) : genres.tvShowGenres.filter(genre => media?.genre_ids?.includes(genre.id));
 
     const navigateToDetailsPage = () => {
         const path = isMovie(media) ? `/movies/${media.id}` : `/tv-shows/${media.id}`;
@@ -28,6 +33,9 @@ const Card: FC<Props> = ({ media }) => {
             <div className="title-wrapper">
                 <h2>{isMovie(media) ? media.title : media.name}</h2>
                 <span><Star />&nbsp;{media.vote_average.toFixed(2)} ({media.vote_count.toLocaleString()})</span>
+            </div>
+            <div className="genres">
+                {filteredGenres?.map(genre => genre.name).join(', ')}
             </div>
             <p>{media.overview}</p>
         </div>
