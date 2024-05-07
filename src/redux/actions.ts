7376d332d28f4dbc3499e25586dd674592b1334e
vascabarkapa <vascabarkapa@gmail.com>
2024-05-07 21:40:09 +0200

    added page global state

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,6 +1,7 @@
 export enum ActionTypes {
     SET_TEXT = 'SET_TEXT',
     SET_TYPE = 'SET_TYPE',
+    SET_PAGE = 'SET_PAGE',
 }
 
 const createAction = <T>(type: ActionTypes, payload: T) => ({
@@ -10,3 +11,4 @@ const createAction = <T>(type: ActionTypes, payload: T) => ({
 
 export const setText = (text: string) => createAction<string>(ActionTypes.SET_TEXT, text);
 export const setType = (type: string) => createAction<string>(ActionTypes.SET_TYPE, type);
+export const setPage = (page: number) => createAction<number>(ActionTypes.SET_PAGE, page);
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -3,11 +3,13 @@ import { ActionTypes } from "./actions";
 export interface TextState {
     text: string;
     type: string;
+    page: number;
 }
 
 const initialState: TextState = {
     text: '',
-    type: ''
+    type: '',
+    page: 1
 };
 
 interface SetTextAction {
@@ -20,7 +22,12 @@ interface SetTypeAction {
     payload: string;
 }
 
-type TextAction = SetTextAction | SetTypeAction;
+interface SetPageAction {
+    type: ActionTypes.SET_PAGE;
+    payload: number;
+}
+
+type TextAction = SetTextAction | SetTypeAction | SetPageAction;
 
 const reducer = (state = initialState, action: TextAction): TextState => {
     switch (action.type) {
@@ -33,6 +40,11 @@ const reducer = (state = initialState, action: TextAction): TextState => {
             return {
                 ...state,
                 type: action.payload,
+            };
+        case ActionTypes.SET_PAGE:
+            return {
+                ...state,
+                page: action.payload,
             };
         default:
             return state;
