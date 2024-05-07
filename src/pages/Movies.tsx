cd31ff976c506ff68143bde9049da5aea9fccc84
vascabarkapa <vascabarkapa@gmail.com>
2024-05-07 21:39:58 +0200

    set pagination

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/root";
-import { setText } from "../redux/actions";
+import { setPage, setText } from "../redux/actions";
 import Card from "../components/Card";
 import SearchService from "../services/searchService";
 import MovieService from "../services/movieService";
@@ -16,15 +16,16 @@ import ReactPaginate from "react-paginate";
 const Movies = () => {
     const dispatch = useDispatch();
     const search = useSelector((state: RootState) => state.text);
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [top10Movies, setTop10Movies] = useState<Movie[]>([]);
+    const page = useSelector((state: RootState) => state.text);
+    const [movies, setMovies] = useState<any>({});
+    const [top10Movies, setTop10Movies] = useState<any>({});
     const [isLoading, setIsLoading] = useState(true);
 
     const fetchSearchResults = useCallback(async () => {
         setIsLoading(true);
         try {
-            const response = await SearchService.searchMoviesbyTitle(search.text);
-            setMovies(response.results);
+            const response = await SearchService.searchMoviesbyTitle(search.text, page.page);
+            setMovies(response);
         } catch (error) {
             console.error('Error fetching search results:', error);
         } finally {
@@ -37,8 +38,9 @@ const Movies = () => {
         try {
             const response = await MovieService.getTopRatedMovies();
             const top10 = response.results.slice(0, 10);
-            setMovies(top10);
-            setTop10Movies(top10);
+            const updatedResponse = { ...response, results: top10 };
+            setMovies(updatedResponse);
+            setTop10Movies(updatedResponse);
         } catch (error) {
             console.error('Error fetching top 10 movies:', error);
         } finally {
@@ -50,17 +52,30 @@ const Movies = () => {
         if (search.text.length >= 3) {
             const timer = setTimeout(fetchSearchResults, 1000);
             return () => clearTimeout(timer);
-        } else if (top10Movies.length > 0) {
+        } else if (top10Movies.results && top10Movies.results.length > 0) {
             setMovies(top10Movies);
             setIsLoading(false);
         } else {
             fetchTop10();
         }
-    }, [search.text, fetchSearchResults, fetchTop10, top10Movies.length, top10Movies]);
+    }, [search.text, fetchSearchResults, fetchTop10, top10Movies]);
 
     const handleInputChange = useCallback((value: string) => {
         dispatch(setText(value));
     }, [dispatch]);
+
+    const handlePageClick = async (event: any) => {
+        setIsLoading(true);
+        try {
+            const response = await SearchService.searchMoviesbyTitle(search.text, event.selected + 1);
+            setMovies(response);
+            dispatch(setPage(event.selected + 1));
+        } catch (error) {
+            console.error('Error fetching search results:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     return (
         <div className='container'>
@@ -70,25 +85,28 @@ const Movies = () => {
             {isLoading ? (
                 <Loading />
             ) : (
-                movies.length > 0 ? (
+                (movies.results && movies.results.length > 0) ? (
                     <>
                         <div className="card-container">
-                            {movies.map(movie => <Card key={movie.id} media={movie} />)}
+                            {movies.results.map((movie: Movie) => <Card key={movie.id} media={movie} />)}
                         </div>
-                        <div className="pagination-container">
+                        {
+                            search.text.length >= 3 &&
+                            <div className="pagination-container">
                                 <ReactPaginate
+                                    forcePage={page.page - 1}
                                     breakLabel="..."
-                                    nextLabel=">>"
-                                    onPageChange={() => console.log('aa')}
+                                    nextLabel={null}
+                                    onPageChange={handlePageClick}
                                     pageRangeDisplayed={5}
-                                    pageCount={5}
-                                    previousLabel="<<"
+                                    pageCount={movies.total_pages}
+                                    previousLabel={null}
                                     containerClassName={"pagination"}
                                     pageClassName={"page-item"}
                                     activeClassName={"active"}
-                                    renderOnZeroPageCount={null}
                                 />
                             </div>
+                        }
                     </>
                 ) : (
                     <SearchNotFound media={'Movies'} />
--- src/pages/TVShows.tsx
+++ src/pages/TVShows.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/root";
-import { setText } from "../redux/actions";
+import { setPage, setText } from "../redux/actions";
 import Card from "../components/Card";
 import SearchService from "../services/searchService";
 import TvShowService from "../services/tvShowService";
@@ -11,19 +11,21 @@ import Tabs from "../components/Tabs";
 import Search from "../components/search/Search";
 import Loading from "../components/loading/Loading";
 import SearchNotFound from "../components/search/SearchNotFound";
+import ReactPaginate from "react-paginate";
 
 const TVShows = () => {
     const dispatch = useDispatch();
     const search = useSelector((state: RootState) => state.text);
-    const [tvShows, setTvShows] = useState<TVShow[]>([]);
-    const [top10TvShows, setTop10TvShows] = useState<TVShow[]>([]);
+    const page = useSelector((state: RootState) => state.text);
+    const [tvShows, setTvShows] = useState<any>({});
+    const [top10TvShows, setTop10TvShows] = useState<any>({});
     const [isLoading, setIsLoading] = useState(true);
 
     const fetchSearchResults = useCallback(async () => {
         setIsLoading(true);
         try {
-            const response = await SearchService.searchTvShowsbyTitle(search.text);
-            setTvShows(response.results);
+            const response = await SearchService.searchTvShowsbyTitle(search.text, page.page);
+            setTvShows(response);
         } catch (error) {
             console.error('Error fetching search results:', error);
         } finally {
@@ -36,10 +38,11 @@ const TVShows = () => {
         try {
             const response = await TvShowService.getTopRatedTvShows();
             const top10 = response.results.slice(0, 10);
-            setTvShows(top10);
-            setTop10TvShows(top10);
+            const updatedResponse = { ...response, results: top10 };
+            setTvShows(updatedResponse);
+            setTop10TvShows(updatedResponse);
         } catch (error) {
-            console.error('Error fetching top 10 tv shows:', error);
+            console.error('Error fetching top 10 TV shows:', error);
         } finally {
             setIsLoading(false);
         }
@@ -49,17 +52,30 @@ const TVShows = () => {
         if (search.text.length >= 3) {
             const timer = setTimeout(fetchSearchResults, 1000);
             return () => clearTimeout(timer);
-        } else if (top10TvShows.length > 0) {
+        } else if (top10TvShows.results && top10TvShows.results.length > 0) {
             setTvShows(top10TvShows);
             setIsLoading(false);
         } else {
             fetchTop10();
         }
-    }, [search.text, fetchSearchResults, fetchTop10, top10TvShows.length, top10TvShows]);
+    }, [search.text, fetchSearchResults, fetchTop10, top10TvShows]);
 
     const handleInputChange = useCallback((value: string) => {
         dispatch(setText(value));
     }, [dispatch]);
+
+    const handlePageClick = async (event: any) => {
+        setIsLoading(true);
+        try {
+            const response = await SearchService.searchTvShowsbyTitle(search.text, event.selected + 1);
+            setTvShows(response);
+            dispatch(setPage(event.selected + 1));
+        } catch (error) {
+            console.error('Error fetching search results:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     return (
         <div className='container'>
@@ -69,10 +85,29 @@ const TVShows = () => {
             {isLoading ? (
                 <Loading />
             ) : (
-                tvShows.length > 0 ? (
-                    <div className="card-container">
-                        {tvShows.map(tvShow => <Card key={tvShow.id} media={tvShow} />)}
-                    </div>
+                (tvShows.results && tvShows.results.length > 0) ? (
+                    <>
+                        <div className="card-container">
+                            {tvShows.results.map((tvShow: TVShow) => <Card key={tvShow.id} media={tvShow} />)}
+                        </div>
+                        {
+                            search.text.length >= 3 &&
+                            <div className="pagination-container">
+                                <ReactPaginate
+                                    forcePage={page.page - 1}
+                                    breakLabel="..."
+                                    nextLabel={null}
+                                    onPageChange={handlePageClick}
+                                    pageRangeDisplayed={5}
+                                    pageCount={tvShows.total_pages}
+                                    previousLabel={null}
+                                    containerClassName={"pagination"}
+                                    pageClassName={"page-item"}
+                                    activeClassName={"active"}
+                                />
+                            </div>
+                        }
+                    </>
                 ) : (
                     <SearchNotFound media={'TV Shows'} />
                 )
